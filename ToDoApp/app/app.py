72e4d9edb708922dcd4530ec93db0f4d86e5a9ab
Kürşad Güzelkaya <55952069+kursadguzelkaya0@users.noreply.github.com>
2023-04-06 14:27:22 +0300

    last updates and analysis page

--- ToDoApp/app/app.py
+++ ToDoApp/app/app.py
@@ -1,6 +1,8 @@
-
 import re  
 import os
+import uuid
+import datetime
+from datetime import timezone, timedelta
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -46,6 +48,7 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        unique_id = uuid.uuid4().int & (1<<32)-1
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM User WHERE username = % s', (username, ))
         account = cursor.fetchone()
@@ -56,7 +59,7 @@ def register():
             message = 'Please fill out the form!'
 
         else:
-            cursor.execute('INSERT INTO User (id, username, email, password) VALUES (NULL, % s, % s, % s)', (username, email, password,))
+            cursor.execute('INSERT INTO User (id, username, email, password) VALUES (%s, % s, % s, % s)', (unique_id, username, email, password,))
             mysql.connection.commit()
             message = 'User successfully created!'
 
@@ -65,13 +68,198 @@ def register():
         message = 'Please fill all the fields!'
     return render_template('register.html', message = message)
 
+@app.route("/logout", methods=['GET', "POST"])
+def logout():
+    session['loggedin'] = False
+    session['userid'] = ""
+    session['username'] = ""
+    session['email'] = ""
+    return redirect(url_for("login"))
+
+##### TASKS #####
 @app.route('/tasks', methods =['GET', 'POST'])
 def tasks():
-    return "Tasks page"
+    message = ''
+    if(request.args.get("message")):
+        message = request.args.get("message")
+    userid = session['userid']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
+    # Get tasks
+    cursor.execute('SELECT * FROM Task WHERE user_id = % s', (userid, ))
+    tasks = cursor.fetchall()
+
+    # Change deadline format
+    for task in tasks:
+        # Format the datetime object to the appropriate format for datetime-local input type
+        task['formattedDeadline'] = task['deadline'].strftime("%Y-%m-%dT%H:%M:%S")
+
+    # Classify as completed and todo
+    completedTasks = []
+    todoTasks = []
+    for task in tasks:
+        if task['status'] == 'Done':
+            completedTasks.append(task)
+        else:
+            todoTasks.append(task)
+
+    # Sort tasks with deadline and done_time
+    completedTasks = sorted(completedTasks, key=lambda task: task['done_time'])
+    todoTasks = sorted(todoTasks, key=lambda task: task['deadline'])
+
+    # Get tasktypes
+    cursor.execute('SELECT * FROM TaskType', ( ))
+    taskTypes = cursor.fetchall()
+
+    # Get task with selected id for edit modal
+    if (request.args.get("task_id")):
+        selectedTaskId = request.args.get("task_id")
+    else:
+        selectedTaskId = "0"
+    selected = tasks[0]
+    for task in tasks:
+        if str(task['id']) == str(selectedTaskId):
+            selected = task
+
+    openCreate = request.args.get("openCreate")
+
+    return render_template('tasks.html', tasks=tasks, completedTasks=completedTasks, todoTasks=todoTasks, taskTypes=taskTypes, message=message, selectedTaskId=selectedTaskId, selectedTask=selected, openCreate=openCreate)
+
+#### CREATE TASK #####
+@app.route("/openCreateModal", methods=["POST", "GET"])
+def openCreateModal():
+    return redirect(url_for("tasks", openCreate="True"))
+
+@app.route("/closeCreateModal", methods=["POST", "GET"])
+def closeCreateModal():
+    return redirect(url_for("tasks", openCreate="False"))
+
+@app.route('/createTask', methods =['GET', 'POST'])
+def createTask():
+    message = ''
+    openCreate = "False"
+
+    userid = session['userid']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    if request.method == 'POST' and 'title' in request.form and 'description' in request.form:
+        title = request.form['title']
+        description = request.form['description']
+        taskType = request.form.get('taskType')
+        dueDate = request.form['dueDate']
+        unique_id = uuid.uuid4().int & (1<<16)-1
+
+        if not title or not description or not taskType or not dueDate:
+            message = 'Fields can not be empty!'
+            openCreate = "True"
+
+        else:
+            creation_time = datetime.datetime.now()
+            creation_time = datetime.datetime.now(tz=timezone.utc)
+            gmt3_offset = timedelta(hours=3)
+            gmt3_timezone = timezone(gmt3_offset)
+            creation_time_gmt3 = creation_time.astimezone(gmt3_timezone)
+
+            cursor.execute('INSERT INTO Task (id, title, description, status, deadline, creation_time, done_time, user_id, task_type) VALUES (%s, %s, %s, %s, %s, %s, NULL, %s, %s)', (unique_id, title, description, "Todo", dueDate, creation_time_gmt3, userid, taskType))
+            mysql.connection.commit()
+    elif request.method == 'POST':
+        print("here")
+
+        message = 'Please fill all the fields! elif'
+    
+    return redirect(url_for("tasks", message=message, openCreate=openCreate))
+
+#### Edit Task ####
+@app.route("/openEditModal/<int:task_id>", methods=["POST", "GET"])
+def openEditModal(task_id):
+    return redirect(url_for("tasks", task_id=task_id))
+
+@app.route("/closeEditModal", methods=["POST", "GET"])
+def closeEditModal():
+    return redirect(url_for("tasks", task_id="0"))
+
+@app.route("/editTask", methods=["POST", "GET"])
+def editTask():
+    message=''
+    selectedTaskId = request.args.get("task_id")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    if request.method == 'POST' and 'edit-title' in request.form and 'edit-description' in request.form:
+        title = request.form['edit-title']
+        description = request.form['edit-description']
+        taskType = request.form.get('edit-taskType')
+        dueDate = request.form['edit-dueDate']
+
+        if not title or not description or not taskType or not dueDate:
+            message = 'Fields can not be empty!'
+            task_id=selectedTaskId
+        else:
+            cursor.execute('UPDATE Task SET title = %s, description = %s, task_type = %s, deadline = %s WHERE id = %s', (title, description, taskType, dueDate, selectedTaskId))
+            mysql.connection.commit()
+            task_id="0"
+    elif request.method == 'POST':
+        message = 'Please fill all the fields! elif'
+
+    return redirect(url_for("tasks", task_id=task_id, message=message))
+
+#### DELETE Task ####
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('DELETE FROM Task WHERE id = %s', (task_id, ))
+    mysql.connection.commit()
+
+    return redirect(url_for("tasks"))
+
+#### MARK TASK ####
+@app.route("/markDone/<int:task_id>")
+def markDone(task_id):
+    done_time = datetime.datetime.now(tz=timezone.utc)
+    gmt3_offset = timedelta(hours=3)
+    gmt3_timezone = timezone(gmt3_offset)
+    done_time_gmt3 = done_time.astimezone(gmt3_timezone)
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('UPDATE Task SET status = %s, done_time = %s  WHERE id = %s', ('Done', done_time_gmt3, task_id, ))
+    mysql.connection.commit()
+
+    return redirect(url_for("tasks"))
+
+@app.route("/markUnDone/<int:task_id>")
+def markUnDone(task_id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('UPDATE Task SET status = %s, done_time = NULL WHERE id = %s', ('Todo', task_id, ))
+    mysql.connection.commit()
+
+    return redirect(url_for("tasks"))
+
+#### ANALYSIS #####
 @app.route('/analysis', methods =['GET', 'POST'])
 def analysis():
-    return "Analysis page"
+    user_id = session['userid']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    
+    # SELECT title, TIMEDIFF(done_time, deadline) as latency, HOUR(TIMEDIFF(done_time, deadline)) DIV 24 as latency_day, HOUR(TIMEDIFF(done_time, deadline)) % 24 as latency_hour, MINUTE(TIMEDIFF(done_time, deadline)) as latency_minute  FROM Task  WHERE user_id=1 AND done_time>deadline;
+    cursor.execute('SELECT title, TIMEDIFF(done_time, deadline) as latency, HOUR(TIMEDIFF(done_time, deadline)) DIV 24 as latency_day, HOUR(TIMEDIFF(done_time, deadline)) %% 24 as latency_hour, MINUTE(TIMEDIFF(done_time, deadline)) as latency_minute  FROM Task  WHERE user_id=%s AND done_time>deadline;', (user_id, ))
+    task_latencies = cursor.fetchall()
+    
+    # SELECT HOUR(S.avg_completion_time) DIV 24 as Day, HOUR(S.avg_completion_time) % 24 as Hour, MINUTE(S.avg_completion_time) as Minute FROM (SELECT SEC_TO_TIME(avg(TIME_TO_SEC(TIMEDIFF(done_time,creation_time)))) as avg_completion_time FROM Task WHERE user_id=1) S; 
+    cursor.execute('SELECT HOUR(S.avg_completion_time) DIV 24 as Day, HOUR(S.avg_completion_time) %% 24 as Hour, MINUTE(S.avg_completion_time) as Minute FROM (SELECT SEC_TO_TIME(avg(TIME_TO_SEC(TIMEDIFF(done_time,creation_time)))) as avg_completion_time FROM Task WHERE user_id=%s) S; ', (user_id, ))
+    avg_completion_time = cursor.fetchall()
+
+    # SELECT task_type, COUNT(*) as count FROM Task WHERE user_id=1 AND status="Done" GROUP BY task_type ORDER BY count DESC;
+    cursor.execute('SELECT task_type, COUNT(*) as count FROM Task WHERE user_id=%s AND status="Done" GROUP BY task_type ORDER BY count DESC;', (user_id, ))
+    completed_tasks_by_tasktypes = cursor.fetchall()
+   
+    # SELECT title, deadline FROM Task WHERE user_id=1 AND status="Todo" ORDER BY deadline ASC;
+    cursor.execute('SELECT title, deadline FROM Task WHERE user_id=%s AND status="Todo" ORDER BY deadline ASC;', (user_id, ))
+    uncompleted_tasks_by_deadline = cursor.fetchall()
+
+    # SELECT * FROM (SELECT title, TIMEDIFF(done_time,creation_time) as completion_time,HOUR(TIMEDIFF(done_time,creation_time)) DIV 24 as Day, HOUR(TIMEDIFF(done_time,creation_time)) % 24 as Hour, MINUTE(TIMEDIFF(done_time,creation_time)) as Minute  FROM Task  WHERE user_id=1 AND status='Done' ORDER BY completion_time DESC LIMIT 2) as S ORDER BY completion_time ASC;
+    cursor.execute('SELECT * FROM (SELECT title, TIMEDIFF(done_time,creation_time) as completion_time,HOUR(TIMEDIFF(done_time,creation_time)) DIV 24 as Day, HOUR(TIMEDIFF(done_time,creation_time)) %% 24 as Hour, MINUTE(TIMEDIFF(done_time,creation_time)) as Minute  FROM Task  WHERE user_id=%s AND status=%s ORDER BY completion_time DESC LIMIT 2) as S ORDER BY completion_time ASC;', (user_id, 'Done', ))
+    completion_time_tasks = cursor.fetchall()
+
+    return render_template('analysis.html', task_latencies=task_latencies, avg_completion_time=avg_completion_time, completed_tasks_by_tasktypes=completed_tasks_by_tasktypes, uncompleted_tasks_by_deadline=uncompleted_tasks_by_deadline, completion_time_tasks=completion_time_tasks)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
--- ToDoApp/schema.sql
+++ ToDoApp/schema.sql
@@ -1,2 +1,54 @@
 CREATE DATABASE IF NOT EXISTS cs353hw4db;
 USE cs353hw4db;
+
+CREATE TABLE User (
+    id INT PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE TaskType (
+    type VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Task (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(255) NOT NULL,
+    deadline DATETIME NOT NULL,
+    creation_time DATETIME NOT NULL,
+    done_time DATETIME,
+    user_id INT NOT NULL,
+    task_type VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+-- Insert users
+INSERT INTO User (id, password, username, email)
+VALUES (1, 'pass123', 'user1', 'user1@example.com'),
+       (2, 'password', 'user2', 'user2@example.com');
+
+-- Insert task types
+INSERT INTO TaskType (type)
+VALUES ('Health'),
+       ('Job'),
+       ('Lifestyle'),
+       ('Family'),
+       ('Hobbies');
+
+
+-- Insert tasks
+INSERT INTO Task (id, title, description, status, deadline, creation_time, done_time, user_id, task_type)
+VALUES
+  (1, 'Go for a walk', 'Walk for at least 30 mins', 'Done', '2023-03-20 17:00:00', '2023-03-15 10:00:00', '2023-03-20 10:00:00', 1, 'Health'),
+  (2, 'Clean the house', 'Clean the whole house', 'Done', '2023-03-18 12:00:00', '2023-03-14 09:00:00', '2023-03-18 17:00:00', 1, 'Lifestyle'),
+  (3, 'Submit report', 'Submit quarterly report', 'Todo', '2023-04-12 17:00:00', '2023-03-21 13:00:00', null, 1, 'Job'),
+  (4, 'Call Mom', 'Call Mom and wish her', 'Todo', '2023-04-06 11:00:00', '2023-03-23 12:00:00', null, 1, 'Family'),
+  (5, 'Gym workout', 'Do weight training for an hour', 'Done', '2023-03-19 14:00:00', '2023-03-12 10:00:00', '2023-03-19 11:00:00', 1, 'Health'),
+  (6, 'Play guitar', 'Learn new song for an hour', 'Todo', '2023-04-05 20:00:00', '2023-03-20 14:00:00', null, 2, 'Hobbies'),
+  (7, 'Book flights', 'Book flights for summer vacation', 'Done', '2023-03-16 09:00:00', '2023-03-13 13:00:00', '2023-03-16 11:00:00', 2, 'Lifestyle'),
+  (8, 'Write a blog post', 'Write about recent project', 'Todo', '2023-04-11 17:00:00', '2023-03-22 09:00:00', null, 2, 'Job'),
+  (9, 'Grocery shopping', 'Buy groceries for the week', 'Todo', '2023-04-05 18:00:00', '2023-03-31 10:00:00', null, 2, 'Family'),
+  (10, 'Painting', 'Paint a landscape for 2 hours', 'Done', '2023-03-23 15:00:00', '2023-03-18 14:00:00', '2023-03-23 16:00:00', 2, 'Hobbies');
